fc6c678c42f43ec2ddd5e8ef91261c3cc63e8e08
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-05-24 16:27:36 -0500

    add stop and record button and navigation control

--- Pitch Changer/Pitch Changer/ViewController.swift
+++ Pitch Changer/Pitch Changer/ViewController.swift
@@ -12,19 +12,30 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var recordingStatus: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        stopRecordButton.isEnabled = false
+    }
 
     @IBAction func recordAudio(_ sender: Any) {
         recordingStatus.text = "Recording in Progress"
+        recordButton.isEnabled = false
+        stopRecordButton.isEnabled = true
     }
     
     
     @IBAction func stopRecording(_ sender: Any) {
         print("Recording stop")
+        recordingStatus.text = "Tap to Record"
+        recordButton.isEnabled = true
+        stopRecordButton.isEnabled = false
     }
 }
 
