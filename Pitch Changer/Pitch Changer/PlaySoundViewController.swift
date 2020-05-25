da49f380ba499bf6baf5989ae6ab5600a9ec6d50
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-05-24 23:04:53 -0500

    add audiorecord code

---
+++ Pitch Changer/Pitch Changer/PlaySoundViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PlaySoundViewController.swift
+//  Pitch Changer
+//
+//  Created by Dhiraj Jain on 5/24/20.
+//  Copyright © 2020 Dhiraj Jain. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundViewController: UIViewController {
+
+    var recordedAudioURL: URL!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
+
--- Pitch Changer/Pitch Changer/RecordAudioViewController.swift
+++ Pitch Changer/Pitch Changer/RecordAudioViewController.swift
@@ -41,6 +41,7 @@ class RecordAudioViewController: UIViewController, AVAudioRecorderDelegate {
         try! session.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.default, options: AVAudioSession.CategoryOptions.defaultToSpeaker)
 
         try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.delegate = self
         audioRecorder.isMeteringEnabled = true
         audioRecorder.prepareToRecord()
         audioRecorder.record()
@@ -57,6 +58,24 @@ class RecordAudioViewController: UIViewController, AVAudioRecorderDelegate {
         audioRecorder.stop()
         let audioSession = AVAudioSession.sharedInstance()
         try! audioSession.setActive(false)
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+       
+        if flag {
+            performSegue(withIdentifier: "stopRecording", sender: audioRecorder.url)
+        } else{
+            print("Recording was not successful")
+        }
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "stopRecording"{
+            let playSoundVC = segue.destination as! PlaySoundViewController
+            let recordedAudioURL = sender as! URL
+            playSoundVC.recordedAudioURL = recordedAudioURL
+        }
     }
 }
 
